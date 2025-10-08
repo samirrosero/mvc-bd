b5a01c58b60c2e9889423f7767187ccb15d6e551
samir <samir_armero16@hotmail.com>
2025-10-07 20:42:42 -0500

    creando archivo config.properties para datos sensibles

--- src/main/java/com/uniajc/mvn/modelo/ConexionDatabase.java
+++ src/main/java/com/uniajc/mvn/modelo/ConexionDatabase.java
@@ -4,22 +4,25 @@ package com.uniajc.mvn.modelo;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
-
+import java.util.Properties;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 public class ConexionDatabase {
     private static Connection connection;
 
     public static Connection getConnection() {
         System.out.println("intentando conectar...");
         if (connection == null) {
+            Properties properties = new Properties();
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-
+                properties.load(new FileInputStream(new File("config.properties")));
                 // variables de conexion
-                String url = "";
-                String user = "";
-                String password = "";
+                String url = properties.getProperty("URL").toString();
+                String user = properties.getProperty("USER").toString();
+                String password = properties.getProperty("PASSWORD").toString();
 
                 // ESTABLECER LA CONEXION
                 connection = DriverManager.getConnection(url, user, password);
@@ -28,6 +31,10 @@ public class ConexionDatabase {
                 System.out.println("Error al cargar el driver de MySQL: " + e.getMessage());
             } catch (SQLException e) {
                 System.out.println("Error al conectar a la base de datos: " + e.getMessage());
+            } catch (FileNotFoundException e) {
+               // System.out.println("Archivo de configuracion no encontrado: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Error al leer el archivo de configuracion: " + e.getMessage());
             }
         }
         return connection;
