b8c2f5c5ba9c5b9da4d5c4a209d9725a274da65e
Valeri-Solis1 <vasolis@estudiante.uniajc.edu.co>
2025-10-10 18:09:49 -0500

    correcciones

--- src/main/java/com/uniajc/mvn/modelo/Estudiante.java
+++ src/main/java/com/uniajc/mvn/modelo/Estudiante.java
@@ -25,10 +25,6 @@ public class Estudiante {
         this.id_estudiante = id_estudiante;
     }
 
-     public Estudiante(String nombre, int edad) {
-        this.nombre = nombre;
-        this.edad = edad;
-    }
     public String getNombre() {
         return this.nombre;
     }
@@ -44,17 +40,19 @@ public class Estudiante {
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
  public static void insertarEstudiante(Estudiante estudiante) {
 
-    String sql = "INSERT INTO estudiante (nombre, edad) VALUES (?, ?)";
+    String sql = "INSERT INTO estudiante (id_estudiante, nombre, edad) VALUES (?, ?, ?)";
 
     try {
       Connection conexion = ConexionDatabase.getConnection();
 
       PreparedStatement preparedStatement = conexion.prepareStatement(sql);
 
-      preparedStatement.setString(1, estudiante.getNombre());
-      preparedStatement.setInt(2, estudiante.getEdad());
+      preparedStatement.setInt(1, estudiante.getId());
+      preparedStatement.setString(2, estudiante.getNombre());
+      preparedStatement.setInt(3, estudiante.getEdad());
 
       // Ejecutar la sentencias SQL INSERT, UPDATE o DELETE
       preparedStatement.executeUpdate();
@@ -69,7 +67,7 @@ public class Estudiante {
 
     List<Estudiante> estudiantes = new ArrayList<>();
 
-    String sql = "SELECT nombre, edad FROM estudiante";
+    String sql = "SELECT id_estudiante, nombre, edad FROM estudiante";
 
     try {
       Connection conexion = ConexionDatabase.getConnection();
@@ -80,9 +78,10 @@ public class Estudiante {
       ResultSet resultSet = statement.executeQuery(sql);
 
       while (resultSet.next()) {
+        int id_estudiante = resultSet.getInt("id_estudiante");
         String nombre = resultSet.getString("nombre");
         int edad = resultSet.getInt("edad");
-        Estudiante estudiante = new Estudiante(nombre, edad);
+        Estudiante estudiante = new Estudiante(id_estudiante,nombre, edad);
         estudiantes.add(estudiante);
       }
 
@@ -95,7 +94,7 @@ public class Estudiante {
   }
 
   public static void actualizarEstudiante(Estudiante estudiante) {
-    String sql = "UPDATE estudiante SET nombre = ?, edad = ? WHERE id = ?";
+    String sql = "UPDATE estudiante SET nombre = ?, edad = ? WHERE id_estudiante = ?";
 
     try {
       Connection conexion = ConexionDatabase.getConnection();
@@ -114,7 +113,7 @@ public class Estudiante {
   }
 
   public static void eliminarEstudiante(int id_estudiante) {
-    String sql = "DELETE FROM estudiante WHERE id = ?";
+    String sql = "DELETE FROM estudiante WHERE id_estudiante = ?";
 
     try {
       Connection conexion = ConexionDatabase.getConnection();
